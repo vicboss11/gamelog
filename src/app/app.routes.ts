c3fadccdfdb6f81116b39feaa427c1f15810baae
Víctor <victor.af1104@gmail.com>
2024-08-27 19:49:22 +0200

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'games',
+    loadComponent: () =>
+      import('./pages/games/games.component').then((m) => m.GamesComponent),
+  },
+];
